5946eb9ae417a1e1a6b0379afcb62a70b56008a6
Caio Satel <caio_satel@hotmail.com>
2025-02-11 13:36:43 -0300

    feat: criando rotas padrões do sistema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { LoginComponent } from './modules/login/login.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent},
-  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }
+  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }, 
+  { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) },
+  // { path: 'projects', loadChildren: () => import('./modules/projects/projects.module').then(m => m.ProjectsModule) },
+  // { path: 'tasks', loadChildren: () => import('./modules/tasks/tasks.module').then(m => m.TasksModule) },
+  // { path: 'releases', loadChildren: () => import('./modules/releases/releases.module').then(m => m.ReleasesModule) }
 ];
 
 @NgModule({
