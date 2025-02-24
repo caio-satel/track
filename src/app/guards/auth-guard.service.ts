eafc3de7943be0a6b971727986514bb39c5fb17f
Caio Satel <caio_satel@hotmail.com>
2025-02-24 20:44:42 -0300

    feat: criando e implementando guarda de rotas

---
+++ src/app/guards/auth-guard.service.ts
@@ -0,0 +1,22 @@
+import { AuthService } from './../services/authentication/auth.service';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router, UrlTree } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+  private AuthService: AuthService = inject(AuthService);
+  private router: Router = inject(Router);
+
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (!this.AuthService.isLoggedIn()) {
+      this.router.navigate(['/']);
+      return false;
+    }
+
+    this.AuthService.isLoggedIn();
+    return true;
+  }
+}
