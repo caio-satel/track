09b6592c2c00b715d880d426a63073b5cf371225
Caio Satel <caio_satel@hotmail.com>
2025-02-24 20:53:24 -0300

    feat: criando e implementando CRUD de usuários v1

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../enviroments/enviroment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserDTO } from '../../DTO/users/userDTO';
+import { AddUserDTO } from '../../DTO/users/addUserDTO';
+import { User } from '../../models/users/user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  http: HttpClient = inject(HttpClient);
+  private apiUrl = `${environment.api_URL}/users`;
 
-  constructor() { }
+  // Crud Users
+  // GET - Listar todos os usuários
+  getUsers(): Observable<UserDTO[]> {
+    return this.http.get<UserDTO[]>(this.apiUrl);
+  }
+
+  // GET - Buscar usuário filtrado por ID
+  getUserById(id: number): Observable<UserDTO> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<UserDTO>(url);
+  }
+
+  // POST - Cadastrar usuário
+  createUser(user: UserDTO): Observable<AddUserDTO> {
+    return this.http.post<AddUserDTO>(this.apiUrl, user);
+  }
+
+  // PUT - Atualizar usuário
+  updateUser(id: number, user: AddUserDTO): Observable<AddUserDTO> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.put<AddUserDTO>(url, user);
+  }
+
+  // DELETE - Deletar usuário
+  deleteUser(id: number): Observable<User> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<User>(url);
+  }
+
 }
