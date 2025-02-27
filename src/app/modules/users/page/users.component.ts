ad902d9809d834c352a37b3532e647636f9f1870
Caio Satel <caio_satel@hotmail.com>
2025-02-26 22:05:56 -0300

    feat: melhorias na responsabilidade de cada componente - user(pai) -> chamar service

--- src/app/modules/users/page/users.component.ts
+++ src/app/modules/users/page/users.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserDTO } from '../../../DTO/users/userDTO';
+import { UsersService } from '../../../services/users/users.service';
+import { SnackbarService } from '../../../shared/snackbar/services/snackbar.service';
+import { UpdateUserDTO } from '../../../DTO/users/updateUserDTO';
+import { Perfil } from '../../../models/enum/perfil.enum';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: UserDTO[] = [];
+
+  constructor(private userServices: UsersService, private snackbar: SnackbarService) { }
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userServices.getUsers().subscribe({
+      next: (response) => {
+        this.users = response;
+      },
+      error: (err) => console.error('Erro ao buscar usuários:', err)
+    });
+  }
+
+  onUserDeleted(userId: number) {
+    this.userServices.deleteUser(userId).subscribe({
+      next: (response) => {
+        if(response) {
+          this.loadUsers();
+          this.users = this.users.filter(user => user.id !== userId);
+        }
+      },
+      error: (err) => console.error('Erro ao excluir o usuário:', err)
+    });
+  }
+
+  onUserAdded(newUser: UserDTO) {
+    this.userServices.createUser(newUser).subscribe({
+      next: (response) => {
+        if(response) {
+          this.loadUsers();
+          this.users = [...this.users, response];
+        }
+      },
+      error: (err) => console.error('Erro ao cadastrar o usuário:', err)
+    });
+  }
+
+  onUserEdited(updatedUser: UpdateUserDTO) {
+    this.userServices.updateUser(updatedUser.id, updatedUser).subscribe({
+      next: () => {
+        this.users = this.users.map(user => {
+          if (user.id === updatedUser.id) {
+            return { ...user, role: updatedUser.role } as UserDTO;
+          }
+          return user;
+        });
+      },
+      error: (err) => console.error('Erro ao atualizar o usuário:', err)
+    });
+  }
+
 
 }
