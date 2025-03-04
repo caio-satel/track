ff63382519f1f0974ec488146f53de1dd7c52626
Caio Satel <caio_satel@hotmail.com>
2025-03-03 23:46:57 -0300

    feat: adicionando novo formcontrol para edição do projeto e melhorias no retorno das datas nos formulários

--- src/app/modules/projects/components/projects-form/projects-form.component.ts
+++ src/app/modules/projects/components/projects-form/projects-form.component.ts
@@ -1,10 +1,13 @@
-import { Component, Inject, inject } from '@angular/core';
+import { Component, Inject} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { Subject } from 'rxjs';
 import { SnackbarService } from '../../../../shared/snackbar/services/snackbar.service';
+import { ProjectDTO } from '../../../../DTO/projects/projectDTO';
+import { Priority } from '../../../../models/enum/priority.enum';
+import { UsersService } from '../../../../services/users/users.service';
 import { UserDTO } from '../../../../DTO/users/userDTO';
-
+import { StatusProject } from '../../../../models/enum/statusProject.enum';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-projects-form',
@@ -12,77 +15,97 @@ import { UserDTO } from '../../../../DTO/users/userDTO';
   styleUrl: './projects-form.component.css'
 })
 export class ProjectsFormComponent {
-  private readonly destroy$: Subject<void> = new Subject();
-  readonly dialogRef = inject(MatDialogRef<ProjectsFormComponent>);
-  isClosing = false;
-  // Armazenar os usuários para preencher a lista de usuários e preencher o campo de usuário responsável
-  users: UserDTO[] = [];
-
-  // Formulário para criação de novo projeto
   newProjectForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    startDate: new FormControl('', [Validators.required]),
-    endDate: new FormControl('', [Validators.required]),
-    userResponsibleId: new FormControl('', [Validators.required]),
+    startDate: new FormControl(null, [Validators.required]),
+    endDate: new FormControl(null, [Validators.required]),
+    responsibleUser: new FormControl('', [Validators.required]),
     priority: new FormControl('', [Validators.required])
   });
 
-  // Injeção de dependência
-  constructor(private snackBar: SnackbarService, @Inject(MAT_DIALOG_DATA) public data: any) { }
+  editProjectForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    startDate: new FormControl(null, [Validators.required]),
+    endDate: new FormControl(null, [Validators.required]),
+    responsibleUser: new FormControl('', [Validators.required]),
+    priority: new FormControl('', [Validators.required]),
+    status: new FormControl('', [Validators.required])
+  });
 
-  // Função executada quando o componente é criado
+  users: UserDTO[] = [];
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private snackBar: SnackbarService,
+    private userService: UsersService,
+    readonly dialogRef: MatDialogRef<ProjectsFormComponent>
+  ) { }
+
   ngOnInit(): void {
-    // Implementar a lógica
-    // Buscar todos os usuários no banco de dados e armazená-los no array users
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        this.users = users;
+      },
+      error: (error) => {
+        console.error('Erro ao carregar usuários:', error);
+      }
+    });
 
-    // Se o projeto está editando um projeto, preencher o formulário com os dados do projeto recebidos no parâmetro data
-    if (this.data && this.data.project) {
-      this.newProjectForm.patchValue({
+    if (this.data?.project) {
+      const startDate = new Date(this.data.project.startDate);
+      const endDate = new Date(this.data.project.endDate);
+
+      const formatDate = (date: Date) => format(date, 'dd/MM/yyyy');
+
+      this.editProjectForm.patchValue({
         name: this.data.project.name,
-        startDate: this.data.project.startDate,
-        endDate: this.data.project.endDate,
-        userResponsibleId: this.data.project.userResponsibleId,
-        priority: this.data.project.priority
+        startDate: formatDate(startDate),
+        endDate: formatDate(endDate),
+        responsibleUser: this.data.project.responsibleUser.id,
+        priority: this.data.project.priority,
+        status: this.data.project.status
       });
     }
   }
 
   addProject(): void {
-    // Posteriormente implementar a lógica chamando o serviço de Projetos
-    // Verificar se o dialog está fechando ou não, para evitar a mensagem do snackbar
-    if (this.isClosing) {
+    if (!this.newProjectForm.valid) {
+      return this.snackBar.openSnackBar('Preencha todos os campos corretamente!', 'warning');
+    }
+    const formValue = this.newProjectForm.value;
+    const formatDate = (date: Date) => format(date, 'dd/MM/yyyy');
+
+    const payload = {
+      name: formValue.name,
+      startDate: formatDate(formValue.startDate),
+      endDate: formatDate(formValue.endDate),
+      responsibleUser: formValue.responsibleUser,
+      priority: formValue.priority
+    };
+
+    this.dialogRef.close(payload);
+  }
+
+  editProject(): void {
+    if (!this.data?.project?.id) {
+      console.error('Projeto não encontrado!');
       return;
     }
-
-    const projectData = this.newProjectForm.value;
-    // Se o formulário estiver inválido, exibe uma mensagem de erro
-    // Se o projeto (data) não existir e o formulário estiver válido, criar um novo projeto
-    // Se o projeto (data) existir e o formulário estiver válido, atualizar o projeto
-    if (!this.newProjectForm.valid || !this.newProjectForm.value) {
-      return this.snackBar.openSnackBar('Preencha todos os campos corretamente!', 'warning')
-
-    } else if (!this.data && this.newProjectForm.valid && this.newProjectForm.value) {
-      console.log(projectData); // Aqui chamaria um serviço de criação
-      this.snackBar.openSnackBar('Cadastro realizado com sucesso!', 'success');
-      this.newProjectForm.reset();
-      this.dialogRef.close();
-      
-    } else {
-      console.log('Editando projeto:', { ...this.data.project, ...projectData }); // Aqui chamaria um serviço de atualização
-      this.snackBar.openSnackBar('Projeto atualizado com sucesso!', 'success');
-      this.newProjectForm.reset();
-      this.dialogRef.close();
+    if (!this.editProjectForm.valid || !this.editProjectForm.value) {
+      return this.snackBar.openSnackBar('Preencha todos os campos corretamente!', 'warning');
     }
-  }
 
-  closeDialog(): void {
-    this.isClosing = true; // Marca que o modal está sendo fechado
-    this.dialogRef.close();
-  }
+    const formValue = this.editProjectForm.value;
 
-  // Função ngOnDestroy
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
+    const updateData: ProjectDTO = {
+      id: this.data.project.id,
+      name: this.editProjectForm.value.name,
+      startDate: formValue.startDate,
+      endDate: formValue.endDate,
+      responsibleUser: +this.editProjectForm.value.responsibleUser,
+      priority: this.editProjectForm.value.priority as Priority,
+      status: this.editProjectForm.value.status as StatusProject
+    };
+    this.dialogRef.close(updateData);
   }
 }
