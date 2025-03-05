713aeb047cc468ae96e8a145f111c93b4bf8cf41
Caio Satel <caio_satel@hotmail.com>
2025-03-04 23:15:01 -0300

    refactor: melhorias no retorno com snackbars, identação do código e formatação

--- src/app/modules/users/page/users.component.ts
+++ src/app/modules/users/page/users.component.ts
@@ -3,7 +3,6 @@ import { UserDTO } from '../../../DTO/users/userDTO';
 import { UsersService } from '../../../services/users/users.service';
 import { SnackbarService } from '../../../shared/snackbar/services/snackbar.service';
 import { UpdateUserDTO } from '../../../DTO/users/updateUserDTO';
-import { Perfil } from '../../../models/enum/perfil.enum';
 
 @Component({
   selector: 'app-users',
@@ -13,7 +12,10 @@ import { Perfil } from '../../../models/enum/perfil.enum';
 export class UsersComponent implements OnInit {
   users: UserDTO[] = [];
 
-  constructor(private userServices: UsersService, private snackbar: SnackbarService) { }
+  constructor(
+    private userServices: UsersService,
+    private snackbar: SnackbarService
+  ) { }
 
   ngOnInit(): void {
     this.loadUsers();
@@ -24,19 +26,7 @@ export class UsersComponent implements OnInit {
       next: (response) => {
         this.users = response;
       },
-      error: (err) => console.error('Erro ao buscar usuários:', err)
-    });
-  }
-
-  onUserDeleted(userId: number) {
-    this.userServices.deleteUser(userId).subscribe({
-      next: (response) => {
-        if(response) {
-          this.loadUsers();
-          this.users = this.users.filter(user => user.id !== userId);
-        }
-      },
-      error: (err) => console.error('Erro ao excluir o usuário:', err)
+      error: () => this.snackbar.openSnackBar('Erro ao buscar usuários!', 'error')
     });
   }
 
@@ -44,11 +34,11 @@ export class UsersComponent implements OnInit {
     this.userServices.createUser(newUser).subscribe({
       next: (response) => {
         if(response) {
-          this.loadUsers();
           this.users = [...this.users, response];
         }
+        this.snackbar.openSnackBar('Usuário cadastrado com sucesso!', 'success');
       },
-      error: (err) => console.error('Erro ao cadastrar o usuário:', err)
+      error: () => this.snackbar.openSnackBar('Erro ao cadastrar usuário!', 'error')
     });
   }
 
@@ -61,10 +51,21 @@ export class UsersComponent implements OnInit {
           }
           return user;
         });
+        this.snackbar.openSnackBar('Usuário atualizado com sucesso!', 'success');
       },
-      error: (err) => console.error('Erro ao atualizar o usuário:', err)
+      error: () => this.snackbar.openSnackBar('Erro ao atualizar usuário!', 'error')
     });
   }
 
-
+  onUserDeleted(userId: number) {
+    this.userServices.deleteUser(userId).subscribe({
+      next: (response) => {
+        if(response) {
+          this.users = this.users.filter(user => user.id !== userId);
+        }
+        this.snackbar.openSnackBar('Usuário excluído com sucesso!', 'success');
+      },
+      error: () => this.snackbar.openSnackBar('Erro ao excluir usuário!', 'error')
+    });
+  }
 }
