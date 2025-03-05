bd5f21ebfd67fca9d6bbb7d43731934034eda4d5
Caio Satel <caio_satel@hotmail.com>
2025-03-05 16:59:23 -0300

    feat: criando e implementando componente lançamento com método para criação

--- src/app/modules/releases/page/releases.component.ts
+++ src/app/modules/releases/page/releases.component.ts
@@ -1,3 +1,8 @@
+import { CreateReleaseDTO } from '../../../DTO/releases/CreateReleaseDTO';
+import { UpdateReleaseDTO } from '../../../DTO/releases/UpdateReleaseDTO';
+import { ReleasesService } from '../../../services/releases/releases.service';
+import { SnackbarService } from '../../../shared/snackbar/services/snackbar.service';
+import { ReleaseDTO } from './../../../DTO/releases/releaseDTO';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +11,35 @@ import { Component } from '@angular/core';
   styleUrl: './releases.component.css'
 })
 export class ReleasesComponent {
+  releases: ReleaseDTO[] = [];
 
+  constructor(
+    private releaseService: ReleasesService,
+    private snackbar: SnackbarService
+  ) { }
+
+  ngOnInit(): void {
+    this.loadReleases();
+  }
+
+  loadReleases() {
+    this.releaseService.getReleases().subscribe({
+      next: (response) => {
+        this.releases = response;
+      },
+      error: () => this.snackbar.openSnackBar('Erro ao buscar lançamentos!', 'error')
+    });
+  }
+
+  onReleaseAdded(newRelease: CreateReleaseDTO) {
+    this.releaseService.createRelease(newRelease).subscribe({
+      next: (response) => {
+        if(response) {
+          this.releases = [...this.releases, response];
+        }
+        this.snackbar.openSnackBar('Lançamento registrado com sucesso!', 'success');
+      },
+      error: () => this.snackbar.openSnackBar('Erro ao registrar lançamento!', 'error')
+    });
+  }
 }
