b084b416920ebcce1233f2c1396584b2ab9ee5e0
Caio Satel <caio_satel@hotmail.com>
2025-03-04 22:44:54 -0300

    feat: melhorias no DTO, retirando campo de responsável e organizando os campos

--- src/app/DTO/tasks/taskDTO.ts
+++ src/app/DTO/tasks/taskDTO.ts
@@ -2,10 +2,9 @@ import { StatusTask } from "../../models/enum/statusTask.enum";
 
 export interface TaskDTO {
   id?: number;
-  projectId: number;
-  userResponsibleId: number;
   name: string;
   startDate: Date;
   endDate: Date;
-  status: StatusTask;
+  status?: StatusTask;
+  projectId: number;
 }
