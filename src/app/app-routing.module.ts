cbb769bd49576026d21e0a410c3bd4947e09523c
Caio Satel <caio_satel@hotmail.com>
2025-03-10 10:40:17 -0300

    feat: adicionando guarda de rotas nos demais paths da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { AuthGuard } from './guards/auth-guard.service';
 const routes: Routes = [
   { path: '', component: LoginComponent},
   { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard] },
-  { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) },
-  { path: 'projects', loadChildren: () => import('./modules/projects/projects.module').then(m => m.ProjectsModule) },
-  { path: 'tasks', loadChildren: () => import('./modules/tasks/tasks.module').then(m => m.TasksModule) },
-  { path: 'releases', loadChildren: () => import('./modules/releases/releases.module').then(m => m.ReleasesModule) }
+  { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule), canActivate: [AuthGuard] },
+  { path: 'projects', loadChildren: () => import('./modules/projects/projects.module').then(m => m.ProjectsModule), canActivate: [AuthGuard] },
+  { path: 'tasks', loadChildren: () => import('./modules/tasks/tasks.module').then(m => m.TasksModule), canActivate: [AuthGuard] },
+  { path: 'releases', loadChildren: () => import('./modules/releases/releases.module').then(m => m.ReleasesModule), canActivate: [AuthGuard] }
 ];
 
 @NgModule({
