2da72dc5e19a61f218e43b4dddb62c51364cd395
Caio Satel <caio_satel@hotmail.com>
2025-03-10 11:48:12 -0300

    feat: criando e implementando formulário/método de edição de um lançamento de horas e melhorias no método de calculo de horas com base no lançamento para funcionar na edição também

--- src/app/modules/releases/components/releases-form/releases-form.component.ts
+++ src/app/modules/releases/components/releases-form/releases-form.component.ts
@@ -20,6 +20,14 @@ export class ReleasesFormComponent {
     taskId: new FormControl('', [Validators.required]),
   });
 
+  editReleaseForm = new FormGroup({
+    description: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    dateRelease: new FormControl(null, [Validators.required]),
+    startTime: new FormControl('', [Validators.required]),
+    endTime: new FormControl('', [Validators.required]),
+    taskId: new FormControl('', [Validators.required]),
+  });
+
   tasks: TaskDTO[] = [];
   hoursReleased: string = '0h';
 
@@ -32,8 +40,29 @@ export class ReleasesFormComponent {
 
   ngOnInit(): void {
     this.listTasksByUser();
-    this.newReleaseForm.get('startTime').valueChanges.subscribe(() => this.calculateHours());
-    this.newReleaseForm.get('endTime').valueChanges.subscribe(() => this.calculateHours());
+    
+    this.newReleaseForm.get('startTime').valueChanges.subscribe(() => this.calculateHours(this.newReleaseForm));
+    this.newReleaseForm.get('endTime').valueChanges.subscribe(() => this.calculateHours(this.newReleaseForm));
+
+    this.editReleaseForm.get('startTime').valueChanges.subscribe(() => this.calculateHours(this.editReleaseForm));
+    this.editReleaseForm.get('endTime').valueChanges.subscribe(() => this.calculateHours(this.editReleaseForm));
+
+    if(this.data?.release) {
+      const dateRelease = this.convertToDate(this.data.release.dateRelease);
+
+      this.editReleaseForm.patchValue({
+        description: this.data.release.description,
+        dateRelease: dateRelease,
+        startTime: this.data.release.startTime,
+        endTime: this.data.release.endTime,
+        taskId: this.data.release.task.id,
+      });
+    }
+  }
+
+  convertToDate(dateString: string): Date {
+    const [day, month, year] = dateString.split('/');
+    return new Date(+year, +month - 1, +day);
   }
 
   // Método para converter "HH:mm" em minutos
@@ -43,9 +72,9 @@ export class ReleasesFormComponent {
   }
 
   // Método para calcular a diferença de horas
-  calculateHours(): void {
-    const startTime = this.newReleaseForm.get('startTime').value;
-    const endTime = this.newReleaseForm.get('endTime').value;
+  calculateHours(form: FormGroup): void {
+    const startTime = form.get('startTime').value;
+    const endTime = form.get('endTime').value;
 
     if (startTime && endTime) {
       // Converte as strings "HH:mm" para minutos
@@ -84,6 +113,30 @@ export class ReleasesFormComponent {
     const formatDate = (date: Date) => format(date, 'dd/MM/yyyy');
 
     const payload = {
+      description: releaseData.description,
+      dateRelease: formatDate(releaseData.dateRelease),
+      startTime: releaseData.startTime,
+      endTime: releaseData.endTime,
+      taskId: releaseData.taskId,
+    };
+
+    this.dialogRef.close(payload);
+  }
+
+  editRelease(): void {
+    if (!this.data?.release?.id) {
+      return this.snackbar.openSnackBar('Lançamento não encontrado no banco de dados!', 'warning');
+    }
+
+    if (!this.editReleaseForm.valid) {
+      return this.snackbar.openSnackBar('Preencha todos os campos corretamente!', 'warning');
+    }
+
+    const releaseData = this.editReleaseForm.value;
+    const formatDate = (date: Date) => format(date, 'dd/MM/yyyy');
+
+    const payload = {
+      id: this.data.release.id,
       description: releaseData.description,
       dateRelease: formatDate(releaseData.dateRelease),
       startTime: releaseData.startTime,
