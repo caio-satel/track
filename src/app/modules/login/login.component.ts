c4410c32ff2de13d24a0bfc8033d95dc3bafeaeb
Caio Satel <caio_satel@hotmail.com>
2025-03-10 11:36:42 -0300

    feat: excluindo formulário de registro e adicionando snackbar de exibição no frontend conforme o status do erro

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -3,9 +3,9 @@ import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SnackbarService } from '../../shared/snackbar/services/snackbar.service';
 import { AuthService } from '../../services/authentication/auth.service';
-import { AddUserDTO } from '../../DTO/users/addUserDTO';
 import { loginRequestDTO } from '../../DTO/users/loginRequestDTO';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -19,19 +19,9 @@ export class LoginComponent {
   CookieService: CookieService = inject(CookieService);
   router: Router = inject(Router);
 
-  // Variavel para esconder/exibir o login/cadastro
-  loginCard = true;
-
   // Formulário de login
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]),
-    password: new FormControl('', [Validators.required])
-  });
-
-  // Formulário de cadastro
-  registerForm = new FormGroup({
-    name: new FormControl ('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required])
   });
 
@@ -47,31 +37,14 @@ export class LoginComponent {
             this.router.navigate(['/dashboard']);
           }
         },
-        error: (error) => {
-          this.snackbar.openSnackBar(error.error.message, 'warning');
-        }
-      });
-    }
-  }
-
-  onSubmitRegister(): void {
-    if (this.registerForm.invalid || !this.registerForm.value) {
-      return this.snackbar.openSnackBar('Preencha todos os campos corretamente!', 'warning');
-    } else {
-      this.authService.register(this.registerForm.value as AddUserDTO).subscribe({
-        next: (response) => {
-          if(response){
-            this.snackbar.openSnackBar('Cadastro realizado com sucesso!', 'success');
-            this.registerForm.reset();
-            this.loginCard = true;
+        error: (err: HttpErrorResponse) => {
+          if (err.status === 401) {
+            this.snackbar.openSnackBar('E-mail ou senha incorretos!', 'error');
+          } else {
+            this.snackbar.openSnackBar('Erro ao realizar login!', 'error');
           }
-        },
-        error: (error) => {
-          console.log(error);
-          this.snackbar.openSnackBar(error.error.message, 'warning');
         }
       });
-      this.registerForm.reset();
     }
   }
 }
