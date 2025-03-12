7523b5dcdde217f07e4e8e8dade7357efefd6a9c
Caio Satel <caio_satel@hotmail.com>
2025-03-11 22:53:19 -0300

    feat: retirando botão de adicionar um integrante na tarefa e refactor do componente typescript

--- src/app/modules/tasks/components/collaborators-modal/collaborators-modal.component.ts
+++ src/app/modules/tasks/components/collaborators-modal/collaborators-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-collaborators-modal',
@@ -7,5 +7,12 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrl: './collaborators-modal.component.css'
 })
 export class CollaboratorsModalComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { collaborators: any[] }) {}
+  collaborators: any[] = [];
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { collaborators: any[] },
+    private dialogRef: MatDialogRef<CollaboratorsModalComponent>
+  ) {
+    this.collaborators = data.collaborators; 
+  }
 }
